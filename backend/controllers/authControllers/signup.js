497dd8e8e3bfb2f369e8bd22ab06de97bfa34092
gbreye <gbrdsmain@gmail.com>
2026-08-13 21:31:04 -0300

    added auth controller

--- backend/controllers/authControllers/signup.js
+++ backend/controllers/authControllers/signup.js
@@ -7,9 +7,9 @@ async function createUser(req, res) {
         if(username.length > 50 || username === '' ) return res.status(400).json({mes: 'invalid body'});
         if(email.length > 50 || username === '' ) return res.status(400).json({mes: 'invalid body'});
         if(password.length > 50 || username === '' ) return res.status(400).json({mes: 'invalid body'});
-
         const cryptPassWd = await bcrypt.hashSync(password, 12);
-
+        await User.create({ username, email, password: cryptPassWd});
+        return res.status(201).json({mes: 'usuario criado com sucesso!'});
     } catch(error) {
         console.log(error);
         return res.status(500).json({mes: 'Server Error'});
