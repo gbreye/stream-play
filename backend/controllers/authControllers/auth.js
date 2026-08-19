c4192f2cbeb815bea784c7fd18ff0648c8d0f820
gbreye <gbrdsmain@gmail.com>
2026-08-18 22:03:18 -0300

    created objects for organization

---
+++ backend/controllers/authControllers/auth.js
@@ -0,0 +1,76 @@
+import bcrypt from "bcrypt";
+import User from "../../models/User.js";
+import jsonwebtoken from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const authController = {
+  login: async function (req, res) {
+    try {
+      const { email, password } = req.body;
+      if (!email || email.length > 50) {
+        return res.status(400).json({ mes: "bad request!" });
+      }
+      if (/[{}]/.test(email)) {
+        return res.status(400).json({ mes: "bad request!" });
+      }
+      if (!password || password.length > 50) {
+        return res.status(400).json({ mes: "bad request!" });
+      }
+      if (/[{}]/.test(password)) {
+        return res.status(400).json({ mes: "bad request!" });
+      }
+      const user = await User.findOne(email);
+      if (!user) return res.status(401).json({ mes: "unable to find user!" });
+      const userPassword = user.password;
+      const compare = await bcrypt.compare(password, userPassword);
+      if (!compare) {
+        console.log("Auth Failed!");
+        return res.status(401).json({ mes: "unable to find user!" });
+      }
+
+      const token = jsonwebtoken.sign(
+        { id: user._id, email: user.email },
+        JWT_SECRET,
+        { expiresIn: "1h" },
+      );
+
+      res.cookie("token", token, {
+        sameSite: "strict",
+        httpOnly: "true",
+        maxAge: "3600000",
+      });
+      return res.status(200).json({ mes: "Login realizado com sucesso!" });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ mes: "Internal Server Error", error });
+    }
+  },
+  signup: async function (req, res) {
+    try {
+      const { username, email, password } = req.body;
+      if (username.length > 50 || username === "")
+        return res.status(400).json({ mes: "invalid body" });
+      if (/[{}]/.test(email)) {
+        return res.status(400).json({ mes: "bad request!" });
+      }
+      if (email.length > 50 || username === "")
+        return res.status(400).json({ mes: "invalid body" });
+      if (password.length > 50 || username === "")
+        return res.status(400).json({ mes: "invalid body" });
+      if (/[{}]/.test(password)) {
+        return res.status(400).json({ mes: "bad request!" });
+      }
+      const cryptPassWd = await bcrypt.hashSync(password, 12);
+      await User.create({ username, email, password: cryptPassWd });
+      return res.status(201).json({ mes: "usuario criado com sucesso!" });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ mes: "Server Error" });
+    }
+  },
+};
+
+export default authController;
--- backend/controllers/contentControllers/upload.js
+++ backend/controllers/contentControllers/upload.js
@@ -1,78 +1,76 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import User from '../../models/User';
-import { createClient } from '@supabase/supabase-js';
-import jsonwebtoken from 'jsonwebtoken';
-import { search } from 'moongose/routes';
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+import User from "../../models/User";
+import { createClient } from "@supabase/supabase-js";
+import jsonwebtoken from "jsonwebtoken";
+import { search } from "moongose/routes";
 const SUPABASE_KEY = process.env.SUPABASE_PUBLISHABLE_KEY;
 const SUPABASE_URL = process.env.SUPABASE_URL;
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+const JWT_SECRET = process.env.JWT_SECRET;
 
-const JWT_SECRET = process.env.JWT_SECRET
+async function saveSong(req, res) {
+  return (saveSong.uploadSong(req, res), saveSong.removeSong(req, res));
+}
 
-async function saveSong(req,res) {
-  return saveSong.uploadSong(req, res), saveSong.removeSong(req,res);
-};
-
-saveSong.uploadSong = async function (req,res) {
+const saveSong = {
+  uploadSong: async function (req, res) {
     const file = req.file;
     const name = req.body.name;
-    const token = req.cookies['token'];
-    if(!token) return res.status(401).json({mes: 'not authorized'});
-    if(file.type !== 'mp3') {
-        return res.status(400).json({mes:'bad request!'});
-    };
-    const fileName = name;
-    const { data, error } = await supabase.storage.createBucket('music', {
-        public: false,
-        allowedMimeTypes: ['music/mp3'],
-    });
-    const { data, error } = await supabase.storage
-        .from('music')
-        .upload(`public/${fileName}`, file.buffer, {
-            cacheControl: '3600',
-            upsert: false,
-            contentType: file.mimetype,
-        });
-    if (error) {
-        console.log('error while uploading the music');
-        return res.status(500).json({mes: 'error while uploading'});
-    };
-    try {
-        const userVerify = jsonwebtoken.verify(token, JWT_SECRET);
-        const id = userVerify.id;
-        const user =  await User.findById(id);
-        const userPlaylist = await user.select('playlist');
-        user.userPlaylist.push(name);
-        await user.save();
-    } catch(error) {
-        return res.status(500).json({mes:''})
+    const token = req.cookies["token"];
+    if (!token) return res.status(401).json({ mes: "not authorized" });
+    if (file.mimetype !== "audio/mpeg") {
+      return res.status(400).json({ mes: "bad request!" });
     }
-};
-
-saveSong.removeSong = async function (req, res) {
+    const fileName = name;
+    const { data, error } = await supabase.storage
+      .from("music")
+      .upload(`public/${fileName}`, file.buffer, {
+        cacheControl: "3600",
+        upsert: false,
+        contentType: file.mimetype,
+      });
+    if (error) {
+      console.log("error while uploading the music");
+      return res.status(500).json({ mes: "error while uploading" });
+    }
+    try {
+      const userVerify = jsonwebtoken.verify(token, JWT_SECRET);
+      const id = userVerify.id;
+      const user = await User.findById(id);
+      user.playlist.push(name);
+      await user.save();
+      return res.status(200).json({ mes: "music added!" });
+    } catch (error) {
+      return res.status(500).json({ mes: "" });
+    }
+  },
+  removeSong: async function (req, res) {
     const name = req.body.name;
     const index = req.body.index;
-    const token = req.cookies['token'];
-    if(!token) return res.status(401).json({mes: 'not authorized'});
+    const token = req.cookies["token"];
+    if (!token) return res.status(401).json({ mes: "not authorized" });
     const fileName = name;
 
     const { data, error } = await supabase.storage
-        .from('music')
-        .remove([fileName]);
+      .from("music")
+      .remove([`public/${fileName}`]);
     if (error) {
-        console.log('error while finding the music');
-        return res.status(500).json({mes: 'error while finding'});
-    };
-    try {
-        const userVerify = jsonwebtoken.verify(token, JWT_SECRET);
-        const id = userVerify.id;
-        const user = await User.findById(id);
-        const userPlaylist = await user.select('playlist');
-        user.userPlaylist.splice(index, 1);
-        await user.save();
-    } catch(error) {
-        console.log('error', error)
-        return res.status(500).json({mes:'internal server error'});
+      console.log("error while finding the music");
+      return res.status(500).json({ mes: "error while finding" });
     }
+    try {
+      const userVerify = jsonwebtoken.verify(token, JWT_SECRET);
+      const id = userVerify.id;
+      const user = await User.findById(id);
+      user.playlist.splice(index, 1);
+      await user.save();
+      return res.status(200).json({ mes: "removed" });
+    } catch (error) {
+      console.log("error", error);
+      return res.status(500).json({ mes: "internal server error" });
+    }
+  },
 };
+
+export default saveSong;
